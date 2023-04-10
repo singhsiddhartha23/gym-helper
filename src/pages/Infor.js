efe3b90c201e429f1027fdb03772f11eb5d8370f
singhsiddhartha23 <130256172+singhsiddhartha23@users.noreply.github.com>
2023-04-11 01:38:34 +0530

    Add files via upload

---
+++ src/pages/Infor.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography, Box, Stack } from "@mui/material";
+
+const Infor = () => {
+  return (
+    <Stack>
+      <Stack>hi</Stack>
+      <Stack>hi1</Stack>
+      <Stack>hi2</Stack>
+    </Stack>
+  );
+};
+
+export default Infor;
